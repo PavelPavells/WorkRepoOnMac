3be07db3dea1069b37815569669b4ae0846f0c38
Павел Смирнов <Pasha@MacBook-Air-Pavel.local>
2019-03-23 15:05:31 +0300

    Initial commit: Add Routing

--- App/TodoApp/todo-app/src/containers/FilterLink.js
+++ App/TodoApp/todo-app/src/containers/FilterLink.js
@@ -1,17 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-import { connect } from 'react-redux'
-import { setVisibilityFilter } from '../actions'
-import Link from '../components/Link'
+const FilterLink = ({ filter, children }) => (
+  <NavLink
+    to={filter === 'SHOW_ALL' ? '/' : `/${filter}`}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black'
+    }}
+  >
+    {children}
+  </NavLink>
+)
 
-const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter
-})
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link)
+export default FilterLink
