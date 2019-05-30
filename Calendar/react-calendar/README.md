2f7b8c621b74a209ad1cb3dd0bf8b4167f0dd127
Павел Смирнов <Pasha@MacBook-Air-Pavel.local>
2019-05-31 00:31:38 +0300

    Initial commit: Add calendar-app, layout

---
+++ Calendar/react-calendar/README.md
@@ -0,0 +1,8 @@
+# React calendar component
+
+To start it in your environment, clone the repo and while in folder, execute following commands
+
+~~~
+npm install OR yarn install
+npm run start OR yarn start
+~~~
---
+++ Calendar/react-calendar/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Calendar from "./components/Calendar";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toggle: true
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange() {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+            <div class="form-group">
+              <div className="header-icon"></div>
+              <input type="text" id="dynamic-label-input" className='header-input' placeholder="Событие, дата или участок" />
+            </div>
+        </header>
+          <button onChange={this.handleChange} class='header-add-button'>Добавить</button>
+          <button class='header-refresh-button'>Обновить</button>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
+//
